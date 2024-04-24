deb40d9e851f7b83c098847273810e662bfc2a0e
Angela <133702261+achenvc2@users.noreply.github.com>
2024-04-24 03:19:31 -0400

    decreasing number of combinations of configs

--- DAEGC/run_experiments.py
+++ DAEGC/run_experiments.py
@@ -16,62 +16,61 @@ config = {
     "num_gat_layers": 2,
 }
 
-MAX_GAT_LAYERS = 6
-
-hidden_sizes = [128, 256, 512, 1024]
+MAX_GAT_LAYERS = 4
 
 hidden_sizes = [
-    [],
-    [128],
+    # [],
+    # [128],
+    # [256],
+    # [512],
+    # [1024],
+    # # [128] * 2,
+    # # [256] * 2,
+    # # [512] * 2,
+    # # [1024] * 2,
+    # [128] * 3,
+    # [256] * 3,
+    # [512] * 3,
+    # [1024] * 3,
+    # # [128] * 4,
+    # # [256] * 4,
+    # # [512] * 4,
+    # # [1024] * 4,
+    # [128] * 5,
+    # [256] * 5,
+    # [512] * 5,
+    # [1024] * 5,
     [256],
-    [512],
-    [128],
-    [256],
-    [512],
-    [128] * 2,
-    [256] * 2,
-    [512] * 2,
-    [1024] * 2,
-    [128] * 3,
-    [256] * 3,
-    [512] * 3,
-    [1024] * 3,
-    [128] * 4,
-    [256] * 4,
-    [512] * 4,
-    [1024] * 4,
-    [128] * 5,
-    [256] * 5,
-    [512] * 5,
-    [1024] * 5,
-    [128] * 6,
-    [256] * 6,
-    [512] * 6,
-    [1024] * 6,
-    [128, 256, 256, 128],
-    [128, 256, 512, 256, 128],
-    [128, 256, 512, 512, 256, 128],
-    [256, 512, 1024, 1024, 512, 256],
+    [256, 256],
+    [256, 512],
+    [1024, 1024],
+    [256, 512, 256],
+    # [128, 256, 256, 128],
+    # [128, 256, 512, 256, 128],
+    # [128, 256, 512, 512, 256, 128],
+    # [256, 512, 1024, 1024, 512, 256],
 ]
 
 configs_finished = []
 
 all_configs = []
 
-for n_clusters in range(1, 11):
-    for update_interval in range(1, 11):
-        for max_epoch in range(100, 301, 100):
-            for epoch in range(50, 101, 5):
-                for num_heads in range(1, 6):
-                    for alpha in [i / 100.0 for i in range(1, 51, 5)]:
-                        for pre_lr in [i / 1000 for i in range(1, 11, 1)]:
-                            for lr in [0.0001, 0.001, 0.0005]:
-                                for embedding_size in [16, 32, 8, 64, 128]:
+num_configs = 0
+
+for n_clusters in [6]:
+    for update_interval in range(1, 8, 2):
+        for max_epoch in range(100, 201, 100):
+            for epoch in range(50, 101, 50):
+                for num_heads in range(1, 5):
+                    for alpha in [i / 100.0 for i in range(10, 31, 10)]:
+                        for pre_lr in [0.005]:
+                            for lr in [0.0001]:
+                                for embedding_size in [16, 32, 8]:
                                     curr_hidden_sizes = [
                                         hidden_size + [embedding_size]
                                         for hidden_size in hidden_sizes
                                         if len(hidden_size) < MAX_GAT_LAYERS
-                                    ] + hidden_sizes
+                                    ]
                                     for curr_hidden_size in curr_hidden_sizes:
                                         config = {
                                             "dataset": "Citeseer",
@@ -88,8 +87,9 @@ for n_clusters in range(1, 11):
                                             "num_heads": num_heads,
                                             "num_gat_layers": len(curr_hidden_size),
                                         }
-                                        all_configs.append(config)
-
+                                        num_configs += 1
+                                        print(num_configs)
+print(num_configs)
 print(all_configs)
 print(len(all_configs))
 
